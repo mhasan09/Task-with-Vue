255789328f1ae62292b3ca74dc0bb506a2d27d72
Mahabubul Hasan <shourav6688@gmail.com>
2021-06-13 14:04:55 +0600

    initial commit

---
+++ src/components/button.vue
@@ -0,0 +1,18 @@
+<template>
+    <button @click="onClick()" :style="{background: color}" class="btn" id="add_task_button" >{{text}}</button>
+</template>
+
+<script>
+export default {
+    name: "Button",
+    props: {
+        text: String,
+        color: String
+    },
+    methods: {
+        onClick(){
+            console.log('llll')
+        }
+    }
+}
+</script>
